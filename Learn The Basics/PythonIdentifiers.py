1b5cfd58983461affbe8ad61d9a86dac035acca9
sahil shaikh <sahilharshalv@gmail.com>
2024-03-31 00:59:08 +0530

    Identifier

---
+++ Learn The Basics/PythonIdentifiers.py
@@ -0,0 +1,14 @@
+'''A Python identifier is a name used to identify a variable, function, class, module or other object.
+ An identifier starts with a letter A to Z or a to z or an underscore (_) followed by zero or more letters,
+   underscores and digits (0 to 9).'''
+
+#Python does not allow punctuation characters such as @, $, and % within identifiers.
+
+
+#Python is a case sensitive programming language. 
+#Thus, Manpower and manpower are two different identifiers in Python.
+
+a = 10 
+A = 20
+print("Print value small a = ",a)
+print("Print the value of Capital A = ",A)
