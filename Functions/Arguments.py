6c3bbc0fa05a7689f24fa910a7fe967e9499a86e
sahil3792 <sahilharshalv@gmail.com>
2024-04-03 16:40:14 +0530

    03 April 2024 Commit

---
+++ Functions/Arguments.py
@@ -0,0 +1,11 @@
+def newfunction(Name):
+    print("My Name is "+Name)
+
+def my_function(FirstName,LastName):
+    print(FirstName+ " James " + LastName)
+
+
+newfunction("Sahil")
+my_function("Emil","Refsnes")
+newfunction("Nilesh")
+
---
+++ Functions/Function.py
@@ -0,0 +1,7 @@
+def my_function(Name):
+    print("Hello, My Name is "+Name)
+
+FirstName = str(input("Enter Your Name:- "))
+
+if FirstName:
+    my_function(FirstName)
