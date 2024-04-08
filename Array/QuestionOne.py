638e395c456ab6d35ce831eeaa5d0b3d2d24ace8
sahil shaikh <sahilharshalv@gmail.com>
2024-04-09 01:49:04 +0530

    09 April 2024

---
+++ Array/QuestionOne.py
@@ -0,0 +1,92 @@
+import numpy
+Size = int(input("Enter Size of Array :- "))
+Array=[]
+
+def RemoveDuplicate(Array):
+    i=0
+    for i in range(0,len(Array)):
+        print("i iteration:- ",i)
+        if i > len(Array):
+            break 
+
+        for j in range(i+1,len(Array)):
+            print("J iteration:- ",j)
+            if j < len(Array):
+                if Array[i] == Array[j]:
+                    Array.pop(j)
+
+            print(Array)
+        
+        
+
+    print(Array)
+
+
+    
+
+for x in range(Size):
+    Array.append(int(input("Enter number to add in Array:- ")))
+
+RemoveDuplicate(Array)
+
+
+#Output
+# Enter Size of Array :- 10
+# Enter number to add in Array:- 8
+# Enter number to add in Array:- 6
+# Enter number to add in Array:- 3
+# Enter number to add in Array:- 2
+# Enter number to add in Array:- 8
+# Enter number to add in Array:- 3
+# Enter number to add in Array:- 6
+# Enter number to add in Array:- 8
+# Enter number to add in Array:- 1
+# Enter number to add in Array:- 6
+# i iteration:-  0
+# J iteration:-  1
+# [8, 6, 3, 2, 8, 3, 6, 8, 1, 6]
+# J iteration:-  2
+# [8, 6, 3, 2, 8, 3, 6, 8, 1, 6]
+# J iteration:-  3
+# [8, 6, 3, 2, 8, 3, 6, 8, 1, 6]
+# J iteration:-  4
+# [8, 6, 3, 2, 3, 6, 8, 1, 6]
+# J iteration:-  5
+# [8, 6, 3, 2, 3, 6, 8, 1, 6]
+# J iteration:-  6
+# [8, 6, 3, 2, 3, 6, 1, 6]
+# J iteration:-  7
+# [8, 6, 3, 2, 3, 6, 1, 6]
+# J iteration:-  8
+# [8, 6, 3, 2, 3, 6, 1, 6]
+# J iteration:-  9
+# [8, 6, 3, 2, 3, 6, 1, 6]
+# i iteration:-  1
+# J iteration:-  2
+# [8, 6, 3, 2, 3, 6, 1, 6]
+# J iteration:-  3
+# [8, 6, 3, 2, 3, 6, 1, 6]
+# J iteration:-  4
+# [8, 6, 3, 2, 3, 6, 1, 6]
+# J iteration:-  5
+# [8, 6, 3, 2, 3, 1, 6]
+# J iteration:-  6
+# [8, 6, 3, 2, 3, 1]
+# J iteration:-  7
+# [8, 6, 3, 2, 3, 1]
+# i iteration:-  2
+# J iteration:-  3
+# [8, 6, 3, 2, 3, 1]
+# J iteration:-  4
+# [8, 6, 3, 2, 1]
+# J iteration:-  5
+# [8, 6, 3, 2, 1]
+# i iteration:-  3
+# J iteration:-  4
+# [8, 6, 3, 2, 1]
+# i iteration:-  4
+# i iteration:-  5
+# i iteration:-  6
+# [8, 6, 3, 2, 1]
+
+
